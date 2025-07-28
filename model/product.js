9225fe14efb021fa0ebc2f33aa92eee36d4494ae
lluy <huymgiyee@gmail.com>
2025-07-28 16:05:39 +0700

    Initial commit

---
+++ model/product.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    offerPrice: {
+        type: Number
+    },
+    proCategoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+    },
+    proSubCategoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SubCategory',
+    },
+    proBrandId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Brand'
+    },
+    proVariantTypeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'VariantType'
+    },
+    proVariantId: [String],
+    images: [{
+        image: {
+            type: Number,
+        },
+        url: {
+            type: String,
+        }
+    }]
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
